fc034ec8dbb7eec227a668e74796b790be4d2da5
Arnold <arnolduwamz7@gmail.com>
2024-05-22 17:13:04 +0300

    updated files

--- Arnold-Weasly-Bolongo/public/js/studentForm.js
+++ Arnold-Weasly-Bolongo/public/js/studentForm.js
@@ -1,19 +1,30 @@
+const validation =  (event)=> {
+  let error = 0;
+
 let firstName = document.getElementById('firstName'); //for input tag id
 let lastName = document.getElementById('lastName'); //for input tag id
-let residence = document.getElementById("residence");
+let course = document.getElementById('course'); //for input tag id
+let entryScheme = document.getElementById('entryScheme'); //for input tag id
+let intake = document.getElementById('intake'); //for input tag id
+let sponsorship = document.getElementById('sponsorship'); //for input tag id
 let gender = document.getElementsByName("gender");
-
+let dob = document.getElementsByName("dob");
+let residence = document.getElementById("residence");
 
 
 
 let firstNameError = document.getElementById('firstNameError'); //for small tag id
 let lastNameError = document.getElementById('lastNameError'); //for small tag id
-let residenceError = document.getElementById("residenceError");
+let courseError = document.getElementById('courseError'); //for small tag id
+let entrySchemeError = document.getElementById('entrySchemeError'); //for small tag id
+let intakeError = document.getElementById('intakeError'); //for small tag id
+let sponsorshipError = document.getElementById('sponsorshipError'); //for small tag id
 let genderError = document.getElementById("genderError");
+let dobError = document.getElementById("dobError");
+let residenceError = document.getElementById("residenceError");
 
 
-const validation = (event) => {
-  let error = 0;
+
 
    //first Name validation
   if (firstName.value == "") {
@@ -40,6 +51,85 @@ const validation = (event) => {
   }
 
 
+   //course validation
+   if (course.value == "") {
+    course.style.border = "1px solid red";
+    courseError.textContent = "Please Select Course Name!";
+    courseError.style = "color: red;";
+    error++;
+    // error = error +1;
+  } else {
+    course.style.border = "1px solid green";
+    courseError.textContent = "";
+  }
+
+
+    //Entry Scheme validation
+    if (entryScheme.value == "") {
+      entryScheme.style.border = "1px solid red";
+      entrySchemeError.textContent = "Please Select entryScheme Name!";
+      entrySchemeError.style = "color: red;";
+      error++;
+      // error = error +1;
+    } else {
+      entryScheme.style.border = "1px solid green";
+      entrySchemeError.textContent = "";
+    }
+
+
+     //Intake validation
+     if (intake.value == "") {
+      intake.style.border = "1px solid red";
+      intakeError.textContent = "Please Select intake Name!";
+      intakeError.style = "color: red;";
+      error++;
+      // error = error +1;
+    } else {
+      intake.style.border = "1px solid green";
+      intakeError.textContent = "";
+    }
+
+
+      //sponsorship validation
+      if (sponsorship.value == "") {
+        sponsorship.style.border = "1px solid red";
+        sponsorshipError.textContent = "Please Select sponsorship Name!";
+        sponsorshipError.style = "color: red;";
+        error++;
+        // error = error +1;
+      } else {
+        sponsorship.style.border = "1px solid green";
+        sponsorshipError.textContent = "";
+      }
+
+
+
+// Gender buttons validation
+if (!formValid) {
+  genderError.textContent = "Please select gender";
+  genderError.style =
+    "color:red; font-size:12px; font-family:Arial, Helvetica, Sans-serif";
+  // return false;
+  error++;
+} else {
+  genderError.textContent = "";
+}
+
+
+ //date of birth validation
+ if (dob.value ==  "") {
+  dob.style.border = "1px solid red";
+  dobError.textContent = "Please enter Date Of Birth!";
+  dobError.style = "color: red;";
+  error++;
+  // error = error +1;
+} else {
+  dob.style.border = "1px solid green";
+  dobError.textContent = "";
+}
+
+
+
    //residence validation
    if (residence.value == "") {
     residence.style.border = "1px solid red";
@@ -51,17 +141,9 @@ const validation = (event) => {
     residence.textContent = " ";
   }
 
-  // Radio buttons validation
-  if (!formValid) {
-    genderError.textContent = "Please select gender";
-    genderError.style =
-      "color:red; font-size:12px; font-family:Arial, Helvetica, Sans-serif";
-    // return false;
-    error++;
-  } else {
-    genderError.textContent = "";
-  }
-  if (error > 0) {
-    event.preventDefault();
-  }
-};
+
+
+ if (error > 0) {
+  event.preventDefault();
+}
+}
